f2041d318288fd1f731f09e5ce11f954c6529c04
jolugoba <59635230+jolugoba@users.noreply.github.com>
2023-01-01 02:49:17 -0500

    Delete writesonic.dart

--- lib/writesonic.dart
+++
@@ -1,4 +0,0 @@
-
-String greeting = "Hola, soy un chat de OpenAI.com y es un gusto saludarte";
-var soyChatSonic = "Hola, soy ChatSonic de Writesonic.com. Estoy aquí para ayudarte con tus necesidades de escritura. ¿En qué puedo ayudarte hoy?";.
-String greeting1 ="Espero que esto te ayude a entender lo que está haciendo el código. ¿Hay algo más en lo que pueda ayudarte?";
