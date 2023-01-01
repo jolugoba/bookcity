f6cab6b0106a830970b20974e9a98751d5574ca5
jolugoba <59635230+jolugoba@users.noreply.github.com>
2023-01-01 02:48:16 -0500

    Update writesonic.dart

--- lib/writesonic.dart
+++ lib/writesonic.dart
@@ -1,3 +1,4 @@
 
 String greeting = "Hola, soy un chat de OpenAI.com y es un gusto saludarte";
 var soyChatSonic = "Hola, soy ChatSonic de Writesonic.com. Estoy aquí para ayudarte con tus necesidades de escritura. ¿En qué puedo ayudarte hoy?";.
+String greeting1 ="Espero que esto te ayude a entender lo que está haciendo el código. ¿Hay algo más en lo que pueda ayudarte?";
